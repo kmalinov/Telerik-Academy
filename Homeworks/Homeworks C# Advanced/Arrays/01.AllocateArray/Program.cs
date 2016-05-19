a59093012af8dfedbd4127efd44b7fe61f8a5d7c
kmalinov <konstantin_malinov@hotmail.com>
2016-05-19 20:37:13 +0300

    Started with C# Advanced Homework

---
+++ Homeworks/Homeworks C# Advanced/Arrays/01.AllocateArray/Program.cs
@@ -0,0 +1,23 @@
+﻿//Problem 01.
+//Write a program that allocates array of N integers, initializes each element by its index multiplied by 5 and the prints the obtained 
+//array on the console.
+
+
+
+using System;
+class Program
+{
+    static void Main()
+    {
+
+        int n = int.Parse(Console.ReadLine());
+        int[] multipleNumbers = new int[n];
+
+        for (int i = 0; i <n; i++)
+        {
+            
+            
+            Console.WriteLine(string.Join("",i*5));
+        }
+    }
+}
---
+++ Homeworks/Homeworks C# Advanced/Arrays/02.CompareArrays/CompareArrays.cs
@@ -0,0 +1,44 @@
+﻿//Problem 2.
+//Write a program that reads two integer arrays of size N from the console and compares them element by element
+//On the first line you will receive the number N
+//On the next N lines the numbers of the first array will be given
+//On the next N lines the numbers of the second array will be given
+
+
+using System;
+using System.Linq;
+
+
+
+class CompareArrays
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int[] first = new int[n];
+        int[] second = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            first[i] = int.Parse(Console.ReadLine());
+            
+           
+        }
+        for (int i = 0; i < n; i++)
+        {
+            second[i] = int.Parse(Console.ReadLine());
+        }
+      
+
+        bool isEqual = Enumerable.SequenceEqual(first, second);
+        if (isEqual == true)
+        {
+            Console.WriteLine("Equal");
+
+        }
+        else
+        {
+            Console.WriteLine("Not equal");
+        }
+         }
+}
