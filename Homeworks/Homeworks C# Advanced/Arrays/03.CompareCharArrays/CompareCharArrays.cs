9fd3863bd996338404b894356a941a9d98774447
kmalinov <konstantin_malinov@hotmail.com>
2016-09-16 13:22:13 +0300

    Added Homework

---
+++ Homeworks/Homeworks C# Advanced/Arrays/03.CompareCharArrays/CompareCharArrays.cs
@@ -0,0 +1,50 @@
+﻿//Problem 3. Compare char arrays
+//Write a program that compares two char arrays lexicographically (letter by letter).
+//Input
+
+//On the first line you will receive the first char array as a string
+//On the second line you will receive the second char array as a string
+//Output
+
+//Print < if the first array is lexicographically smaller
+//Print > if the second array is lexicographically smaller
+//Print = if the arrays are equal
+
+
+
+using System;
+using System.Linq;
+using System.Collections.Generic;
+class CompareCharArrays
+{
+    static void Main()
+    {
+
+        string arr1 = Console.ReadLine();
+        string arr2 = Console.ReadLine();
+
+     
+        char[] firstA = arr1.ToCharArray();
+        char[] secondA = arr2.ToCharArray();
+
+        if (string.Join(" ", arr1) == string.Join(" ", arr2))
+        {
+            Console.Write("=");
+            
+        }
+
+
+        else if (string.Join("", arr1) != string.Join("", arr2) && firstA.Length > secondA.Length)
+        {
+            Console.Write(">");
+        }
+
+        else if (string.Join("", arr1) != string.Join("", arr2) && firstA.Length < secondA.Length)
+        {
+            Console.Write("<");
+        }
+
+  
+
+    }
+}
---
+++ Homeworks/Homeworks C# Advanced/Arrays/04.MaximalSequence/MaximalSequence.cs
@@ -0,0 +1,47 @@
+﻿//Problem 04. Maximal sequence
+//Write a program that finds the length of the maximal sequence of equal elements in an array of N integers.
+
+//Input
+
+//On the first line you will receive the number N
+//On the next N lines the numbers of the array will be given
+
+
+
+
+using System;
+class MaximalSequence
+
+
+{
+    static void Main()
+    {
+        //int n = int.Parse(Console.ReadLine());
+        //int sum=0;
+        //int j = 0;
+        //int count = 0;
+        //int[] array = new int[n];
+        //for (int i = 0; i < array.Length; i++)
+        //{
+        //    array[i] = int.Parse(Console.ReadLine());
+
+            
+        //}
+        //for (int i = 0; i < array.Length; i++)
+        //{
+        //    j=i;
+        //    if (j==1)
+        //    {
+        //        sum += j;
+        //        count++;
+               
+        //    }
+
+
+        //}
+
+      //  Console.WriteLine(count);
+   
+        
+    }
+}
