e07f181d1053a21cc197bc5ba8696f3ef8fd74ef
kmalinov <konstantin_malinov@hotmail.com>
2016-09-19 00:11:48 +0300

    Added problem 11 - Binary To Decimal

---
+++ Homeworks/Homeworks C#1/Loops/11.BinaryToDecimal/BinaryToDecimal.cs
@@ -0,0 +1,25 @@
+﻿//Binary to Decimal
+
+//Description
+//Using loops write a program that converts a binary integer number to its decimal form.
+//The input is entered as string. The output should be a variable of type long.
+
+
+
+using System;
+class BinaryToDecimal
+{
+    static void Main()
+    {
+        string binary = Console.ReadLine();
+        int sum = 0;
+
+        foreach (char bit in binary)
+        {
+            sum = (bit - '0') + sum * 2;
+        }
+
+
+        Console.WriteLine(sum);
+    }
+}
