4fb82ea9d6d1a7bf85a320b87e9e12ee16dd98a4
kmalinov <konstantin_malinov@hotmail.com>
2016-09-25 13:35:47 +0300

    added another solution for problem.02 - Compare arrays

--- Homeworks/Homeworks C# Advanced/Arrays/02.CompareArrays/CompareArrays.cs
+++ Homeworks/Homeworks C# Advanced/Arrays/02.CompareArrays/CompareArrays.cs
@@ -14,6 +14,67 @@ class CompareArrays
 {
     static void Main()
     {
+        int N = int.Parse(Console.ReadLine());
+        bool isEqual = true;
+        int[] arrA = new int[N];
+        int[] arrB = new int[N];
+
+        for (int i = 0; i < N; i++)
+        {
+            arrA[i] = int.Parse(Console.ReadLine());
+        }
+
+        for (int i = 0; i < N; i++)
+        {
+            arrB[i] = int.Parse(Console.ReadLine());
+        }
+        for (int i = 0; i < N; i++)
+        {
+            if (arrA[i]!=arrB[i])
+            {
+                isEqual = false;
+            }
+
+        }
+        if (isEqual == true)
+        {
+            Console.WriteLine("Equal");
+        }
+        else
+        {
+            Console.WriteLine("Not equal");
+        }
+
+        /* added second solution
+          int n = int.Parse(Console.ReadLine());
+        int[] first = new int[n];
+        int[] second = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            first[i] = int.Parse(Console.ReadLine());
+            
+           
+        }
+        for (int i = 0; i < n; i++)
+        {
+            second[i] = int.Parse(Console.ReadLine());
+        }
       
+
+        bool isEqual = Enumerable.SequenceEqual(first, second);
+        if (isEqual == true)
+        {
+            Console.WriteLine("Equal");
+
+        }
+        else
+        {
+            Console.WriteLine("Not equal");
+        }
+         
+         
+         
+         */
     }
 }
