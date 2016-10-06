8a671040d28b7fceb48186d29c21a2cc33e836bd
kmalinov <konstantin_malinov@hotmail.com>
2016-10-06 13:53:58 +0300

    solved new problem - 04. Maximal sequence

--- Homeworks/Homeworks C# Advanced/Arrays/04.MaximalSequence/MaximalSequence.cs
+++ Homeworks/Homeworks C# Advanced/Arrays/04.MaximalSequence/MaximalSequence.cs
@@ -16,10 +16,39 @@ class MaximalSequence
 {
     static void Main()
     {
-   
 
-       
+        int counter = 0;
+        int maxConter = 0;
         
+        int a = int.Parse(Console.ReadLine());
         
+        int[] arr = new int[a];
+        int b =0;
+        for (int i = 0; i < arr.Length; i++)
+        {
+            
+            arr[i] = int.Parse(Console.ReadLine());
+           
+            
+
+            if (b==arr[i])
+            {
+                counter++;
+                maxConter = counter;
+                
+            }
+            b = arr[i];
+           
+        }
+
+        if (arr[0]==0)
+        {
+            Console.WriteLine(counter);
+        }
+
+        else
+        {
+            Console.WriteLine(counter+1);
+        }
     }
 }
