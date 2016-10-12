2247c40318e8be62d915f7a8e630a554fee1b254
kmalinov <konstantin_malinov@hotmail.com>
2016-10-12 23:15:54 +0300

    Added new Project

---
+++ Homeworks/Homeworks C# Advanced/CSProjects/TextToVoice/TextToVoice/TextToVoice.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Speech.Synthesis;
+class TextToVoice
+{
+
+    static void Main(string[] args)
+    {
+        SpeechSynthesizer synthesizer = new SpeechSynthesizer();
+        synthesizer.Volume = 100;  // 0...100
+        synthesizer.Rate = -2;     // -10...10
+
+        // Synchronous
+        synthesizer.Speak(Console.ReadLine());
+
+    }
+}
