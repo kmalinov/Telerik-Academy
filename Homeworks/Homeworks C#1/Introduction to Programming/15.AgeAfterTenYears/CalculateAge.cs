b1b8b09a8bb9da6e93ad5ef94db774d8cfb70c9b
kmalinov <konstantin_malinov@hotmail.com>
2016-10-17 13:03:39 +0300

    Fixed Problem 15.AgeAfterTenYears

--- Homeworks/Homeworks C#1/Introduction to Programming/15.AgeAfterTenYears/CalculateAge.cs
+++ Homeworks/Homeworks C#1/Introduction to Programming/15.AgeAfterTenYears/CalculateAge.cs
@@ -22,33 +22,60 @@
 
 
 //        }
+//using System;
+//class Zadacha11
+//{
+
+//    static void Main()
+//    {
+//        string dateOfBirth = Console.ReadLine();
+//        DateTime today = DateTime.Today;
+//        int yearOfBirth = int.Parse(dateOfBirth.Substring(6, 4));
+//        int monthOfBirth = int.Parse(dateOfBirth.Substring(3, 2));
+//        int todayDay = int.Parse(dateOfBirth.Substring(0, 2));
+//        int myAge = today.Year - yearOfBirth;
+
+//        if (monthOfBirth <= today.Month && todayDay <= today.Day)
+//        {
+//            Console.WriteLine(myAge);
+//            Console.WriteLine(myAge + 10);
+
+//        }
+
+//        else
+//        {
+//            Console.WriteLine(myAge - 1);
+//            Console.WriteLine(myAge + 9);
+//        }
+//    }
+//}
+
 using System;
-class Zadacha11
+using System.Threading;
+using System.Globalization;
+
+class Age
 {
-
-    static void Main()
+    static void Main(string[] args)
     {
-        string dateOfBirth = Console.ReadLine();
+        string dateStr = Console.ReadLine();
+        DateTime date = DateTime.ParseExact(dateStr, "MM.dd.yyyy", CultureInfo.InvariantCulture);
+        //  Console.WriteLine(date.ToString("dd.MM.yyyy"));
         DateTime today = DateTime.Today;
-        int yearOfBirth = int.Parse(dateOfBirth.Substring(6, 4));
-        int monthOfBirth = int.Parse(dateOfBirth.Substring(3, 2));
-        int todayDay = int.Parse(dateOfBirth.Substring(0, 2));
-        int myAge = today.Year - yearOfBirth;
-
-        if (monthOfBirth <= today.Month && todayDay <= today.Day)
+        if (date.Day > today.Day)
         {
-            Console.WriteLine(myAge);
-            Console.WriteLine(myAge + 10);
-
+            Console.WriteLine("{0:F0}", ((today - date).Days / 365.25) - 1);
+            Console.WriteLine("{0:F0}", ((today - date).Days / 365.25) + 9);
         }
 
         else
         {
-            Console.WriteLine(myAge - 1);
-            Console.WriteLine(myAge + 9);
+            Console.WriteLine("{0:F0}", (today - date).Days / 365.25);
+            Console.WriteLine("{0:F0}", ((today - date).Days / 365.25) + 10);
+
         }
+
+
     }
 }
-    
-
 
