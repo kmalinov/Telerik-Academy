81d528ed3ad7ccd0fcc968d882c0adf935fe807d
kmalinov <konstantin_malinov@hotmail.com>
2016-10-26 16:03:45 +0300

    Added two proplems in Methods.sln

---
+++ Homeworks/Homeworks C# Advanced/Methods/01.SayHello/SayHello.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        SayHallo();
+
+    }
+
+
+    static void SayHallo()
+    {
+
+       string name = Console.ReadLine();
+        Console.WriteLine("Hello {0}!",name);
+      
+    }
+}
---
+++ Homeworks/Homeworks C# Advanced/Methods/02.GetLargestNumber/GetLargestNumber.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+class GetLargestNumber
+{
+    static void Main()
+    {
+        int[] j = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+        int a = j[0];
+        int b = j[1];
+        int c = j[2];
+
+        
+        if (GetMax(a,b)>c)
+        {
+            Console.WriteLine(GetMax(a,b));
+        }
+
+        else
+        {
+            Console.WriteLine(c);
+        }
+
+
+
+    }
+    static int GetMax(int a, int b)
+    {
+
+        return(Math.Max(a,b));
+    }
+
+}
+
+
