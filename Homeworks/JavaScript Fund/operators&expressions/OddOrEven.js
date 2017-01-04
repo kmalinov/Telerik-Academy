f48f913da53484e9b4af308a40707e0e2f37f188
kmalinov <konstantin_malinov@hotmail.com>
2017-01-04 17:17:55 +0200

    Started with JavaScript Fundamentals - homework

---
+++ Homeworks/JavaScript Fund/operators&expressions/OddOrEven.js
@@ -0,0 +1,26 @@
+function solve(integerA) {
+
+
+    if (integerA % 2 === 0) {
+        console.log('even', parseInt(integerA));
+
+    } else {
+        console.log('odd', parseInt(integerA));
+
+    }
+}
+
+
+//function solve(integerA) {
+
+
+//if (integerA % 2 === 0) {
+// console.log('even ' + integerA);
+
+// } else {
+//        console.log('odd ' + integerA);
+
+//   }
+//}
+solve(17);
+solve(20);
